acbe4f1597a063fdbc199705950f8a8dbbf2b44e
Fernnado Zimmermann Leite <fzleite@gmail.com>
2018-02-28 10:54:52 -0300

    Ultima atualização

---
+++ js/common.js
@@ -0,0 +1,163 @@
+/**
+ * Lista de Usuarios
+ */
+var userList = [
+        { 
+            "user": "fleite",
+            "name": "Fernando Zimmermann Leite",
+            "id_user": 2,
+            "email": "fleite@artit.com.br",
+            "profileImage": "images/fleite.png"
+        },
+        { 
+            "user": "cmenezes",
+            "name": "Ciro de Menezes",
+            "id_user": 33,
+            "email": "cmenezes@artit.com.br",
+            "profileImage": "images/cmenezes.jpg"
+        },
+        { 
+            "user": "romulo",
+            "name": "Romulo Cesar de Paula",
+            "id_user": 7,
+            "email": "romulo@artit.com.br",
+            "profileImage": "images/romulo.jpg"
+        },
+        { 
+            "user": "lardito",
+            "name": "Luis Afonso Ardito",
+            "id_user": 55,
+            "email": "lardito@artit.com.br",
+            "profileImage": "images/lardito.jpg"
+        },
+        { 
+            "user": "mmelari",
+            "name": "Monica Melari",
+            "id_user": 54,
+            "email": "mmelari@artit.com.br",
+            "profileImage": "images/mmelari.jpg"
+        }
+    ];
+   
+ /**
+  * Lista de Links
+  */
+ var linkPage = {
+         home               : 0,
+         add_opportunity    : 1,
+         cons_opportunity   : 2,
+         add_customer       : 3,
+         cons_customer      : 4,
+         reports            : 5,
+         add_rh             : 6,
+         cons_rh            : 7,
+         feedback           : 8,
+         refund             : 9,
+         cons_net           : 10,
+         profile            : 11
+ } 
+    
+/**
+ * Controles do Angular
+ */
+function usersController($scope) {
+     $scope.itens = userList;
+}
+
+function profileController($scope) {
+    var userData = getUserData( getUserCookie() )[0];
+
+    if( typeof userData != 'object' ){
+        userData = userList[0];
+    }
+
+    $scope.username     = userData.user;
+    $scope.profileName  = userData.name;
+    $scope.profileEmail = userData.email;
+    $scope.profileImage = userData.profileImage;
+}
+
+/**
+ * Funções para tratamento de cookie
+ */
+
+function setUserCookie(user){
+    document.cookie = "user=" + user;
+    window.location.reload();
+}
+
+function getUserCookie(){
+    var ret = document.cookie;
+    if( ret != "" )
+        ret = ret.split("=")[1];
+
+    return ret;
+}
+
+/**
+ * Funções de Localização do JSON
+ */
+function getUserData( username ) {
+  return userList.filter(
+      function( userList ){ 
+          return userList.user == username;
+      }
+  );
+}
+
+/**
+ * Funções de Navegação
+ */
+function home(){
+    $(frameContent).addClass('hide');
+    $(cardsContent).removeClass('hide');
+}
+
+function showContent(){
+    $(frameContent).removeClass('hide');
+    $(cardsContent).addClass('hide');
+}
+
+function openLink( id ){
+    var url;    
+    
+    if( id == linkPage.home ){ // HOME
+        home();
+    }else{
+        if( id == linkPage.add_opportunity ){ // Aduciobar Oportunidade
+            url = "https://rosie.artit.com.br/pipeline/pipeline/incluir";
+        }else if( id == linkPage.cons_opportunity ){ // Consultar Oportunidade
+            url = "https://rosie.artit.com.br/pipeline/pipeline/consultar?projeto=0&date_start=&date_end=&probabilidade=0&fase%5B%5D=1&fase%5B%5D=2&fase%5B%5D=3&responsavel=" + getUserData( getUserCookie() )[0].id_user;
+        }else if( id == linkPage.add_customer ){ // Incluir Cliente
+            url = "https://rosie.artit.com.br/pipeline/cliente/incluir";
+        }else if( id == linkPage.cons_customer ){ // Consultar Cliente
+            url = "https://rosie.artit.com.br/pipeline/cliente/consultar";
+        }else if( id == linkPage.reports ){ // Relatorios
+            url = "https://rosie.artit.com.br/pipeline/relatorio";
+        }else if( id == linkPage.add_rh ){ // Adicionar Vagas
+            url = "https://rosie.artit.com.br/rh/recrutamento/vagas/nova";
+        }else if( id == linkPage.cons_rh ){ // Consultar Vagas
+            url = "https://rosie.artit.com.br/rh/recrutamento/vagas";
+        }else if( id == linkPage.feedback ){ // Feedbacks
+            url = "https://rosie.artit.com.br/rh/feedback";
+        }else if( id == linkPage.refund ){ // Reembolso
+            url = "https://rosie.artit.com.br/user/perfil?panel=reembolso";
+        }else if( id == linkPage.cons_net ){ // Consultar Oportunidades da NET
+            url = "https://rosie.artit.com.br/pipeline/pipeline/consultar?cliente=2489&projeto=0&date_start=&date_end=&probabilidade=0&fase%5B%5D=1&fase%5B%5D=2&fase%5B%5D=3&responsavel=" + getUserData( getUserCookie() )[0].id_user;
+        }else if( id == linkPage.profile ){ // Profile do Rosie
+            url = "https://rosie.artit.com.br/user/perfil";
+        }
+        showContent();
+        $('#frmContent').attr('src', url);
+    } 
+    $('#sideMenu').sideNav('hide');
+}
+
+/**
+ * Registro do Service Worer
+ */
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(function() { console.log('Service Worker Registered'); });
+}
---
+++ service-worker.js
@@ -0,0 +1,72 @@
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'pipeline-v1';
+var cacheName = 'ArtITPipeline-v1';
+var filesToCache = [
+  '/',
+  'index.html',
+  'js/common.js',
+  'js/materialize.min.js',
+  'css/materialize.min.css',
+  'images/cmenezes.jpg',
+  'images/romulo.jpg',
+  'images/fleite.png',
+  'images/lardito.jpg',
+  'images/mmelari.jpg',
+  'images/artit_bkg.png',
+  'images/clientes.jpg',
+  'images/oportunidade.jpg',
+  'images/reembolso.jpg',
+  'images/relatorios.jpg',
+  'images/rh.jpg',
+  'images/ico/android-icon-192x192.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] PIPELINE SW: Instalando PIPELINE Service Worker');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] PIPELINE SW: Criando Cache');
+      return cache.addAll(filesToCache);
+    }).catch(function(err) {
+      console.log('[ServiceWorker] PIPELINE SW: ERRO - ' + err);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
+});
